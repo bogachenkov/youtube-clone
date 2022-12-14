8cd9acd5cb0c8bdd0e6f2b07a2e549fe67a2a6f8
Nikita Bogachenkov <deskotno@gmail.com>
2022-12-14 19:19:11 +0300

    fix url problem

--- lib/queries/getVideos.ts
+++ lib/queries/getVideos.ts
@@ -1,11 +1,13 @@
 import axios from "axios"
 import { IVideo } from "../../types/Video";
+import { getServerUrl } from "../utils/getServerUrl";
 
 interface IVideoResponse {
   videos: IVideo[];
 }
 
 export const getVideos = async () => {
-  const response = await axios.get<IVideoResponse>('http://localhost:3000/api/videos');
+  const serverUrl = getServerUrl();
+  const response = await axios.get<IVideoResponse>(`${serverUrl}/api/videos`);
   return response.data.videos;
 }
